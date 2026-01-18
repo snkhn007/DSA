5d3ffc022cc0e1c6c4330599d557bd0ade18cc9b
Sana <sanasadafkhan2005@gmail.com>
2026-01-18 18:48:34 +0530

    Create middleOfList.cpp

---
+++ LinkedList/Problems/middleOfList.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+void insertAtStart(Node*& head, int data) {
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void display(const Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void freeSpace(Node*& head) {
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+int count(Node* head){
+    int count = 0;
+    while(head != nullptr){
+        count++;
+        head = head -> next;
+    }
+    return count;
+}
+Node* middleElement(Node* head){
+    if(head == nullptr) return nullptr;
+    int ind = 1;
+    int pos = (count(head)/2)+1;
+    while(ind < pos){
+        head = head->next;
+        ind++;
+    }
+    return head;
+}
+int main() {
+    Node* head = nullptr;
+
+    // 1,2,3,4,5
+    insertAtStart(head, 5);
+    insertAtStart(head, 4);
+    insertAtStart(head, 3);
+    insertAtStart(head, 2);
+    insertAtStart(head, 1);
+    insertAtStart(head, 10);
+
+    display(head);
+    Node* midRes = middleElement(head);
+    if(midRes) cout<<"Middle element: "<<midRes->data<<endl;
+
+    freeSpace(head);
+    return 0;
+}
