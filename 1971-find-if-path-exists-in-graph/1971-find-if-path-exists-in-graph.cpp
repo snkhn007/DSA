6a2e74746f4d0ed8c048e7f49ccbf0d10e78739d
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-10 21:15:21 +0530

    Time: 400 ms (35.86%), Space: 311.9 MB (39.12%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        // generic bfs but on each node see if it is equal to destination or not
-        if(source == destination) return true;
-
-        queue <int> q;
-        vector<bool> vis(n, false);
-
         vector<vector<int>> adj(n);
-        for(auto i : edges) {
+        for(auto i : edges){
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
 
+        queue<int> q;
+        vector<bool> vis(n, false);
 
         q.push(source);
         vis[source] = true;
+
         while(!q.empty()){
-            int current = q.front();
+            int currentKey = q.front();
             q.pop();
-            cout<<current<<" ";
-            for(auto i: adj[current]){
-                if(i == destination) return true;
+            if(destination == currentKey) return true;
+
+            for(auto i:adj[currentKey]){
+                // if(destination == i) return true;
                 if(!vis[i]){
                     q.push(i);
                     vis[i] = true;
