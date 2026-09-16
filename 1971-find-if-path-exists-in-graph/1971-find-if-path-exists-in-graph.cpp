9e7ca6555cc02a8e301682cb696ece4fa602467c
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-16 15:47:04 +0530

    Time: 399 ms (37.65%), Space: 311.6 MB (44.53%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        if(source == destination) return true;
+
+        int size = edges.size();
         vector<vector<int>> adj(n);
-        for(auto i : edges){
+        for(auto i: edges){
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
 
-        queue<int> q;
-        vector<bool> vis(n, false);
+        // for(int i = 0; i < adj.size(); i++){
+        //     cout << i << " -> ";
 
+        //     for(int j = 0; j < adj[i].size(); j++){
+        //         cout << adj[i][j] << " ";
+        //     }
+
+        //     cout << endl;
+        // }
+        queue <int> q;
+        vector<bool> vis(n, false);
         q.push(source);
         vis[source] = true;
 
         while(!q.empty()){
-            int currentKey = q.front();
+            int curr = q.front();
             q.pop();
-            if(destination == currentKey) return true;
-
-            for(auto i:adj[currentKey]){
-                // if(destination == i) return true;
+            for(auto i:adj[curr]){
+                if( i == destination) return true;
                 if(!vis[i]){
                     q.push(i);
                     vis[i] = true;
                 }
             }
         }
+
         return false;
     }
 };
