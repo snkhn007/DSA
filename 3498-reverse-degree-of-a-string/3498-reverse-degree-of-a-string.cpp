e3602388c0d9b5cf9beeb4b80301297279890d3c
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-20 09:25:24 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,39 +1,13 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        map<char, int> mp = {
-            {'a', 26},
-            {'b', 25},
-            {'c', 24},
-            {'d', 23},
-            {'e', 22},
-            {'f', 21},
-            {'g', 20},
-            {'h', 19},
-            {'i', 18},
-            {'j', 17},
-            {'k', 16},
-            {'l', 15},
-            {'m', 14},
-            {'n', 13},
-            {'o', 12},
-            {'p', 11},
-            {'q', 10},
-            {'r', 9},
-            {'s', 8},
-            {'t', 7},
-            {'u', 6},
-            {'v', 5},
-            {'w', 4},
-            {'x', 3},
-            {'y', 2},
-            {'z', 1}
-        };
 
         int res = 0;
         for(int i=0; i<s.length(); i++){
+            int revVal = 26 - (s[i] - 'a');
+            res += revVal*(i+1);
             // cout<<mp[s[i]];
-            res += (mp[s[i]] * (i+1));
+            // res += (mp[s[i]] * (i+1));
         }
         return res;
     }
