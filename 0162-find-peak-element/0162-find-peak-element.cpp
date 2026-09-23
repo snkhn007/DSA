71033150e1734d2b0dcefc961261e4d307e3f56f
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-23 10:34:31 +0530

    Time: 0 ms (100%), Space: 12.6 MB (54.33%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int strt = 0, end = arr.size()-1;
+        while(strt < end){
+            int mid = strt + (end - strt)/2;
+            if(arr[mid]<arr[mid+1]) strt = mid+1;
+            else end = mid;
+        }
+        return end;
+        
+    }
+};
