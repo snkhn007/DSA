cb2734537526def60bb781aab75b7eac56ec262a
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-24 09:10:14 +0530

    Time: 0 ms (100%), Space: 17.1 MB (38.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,65 +1,51 @@
 class Solution {
 public:
-    int bfs(queue<pair<int, int>>& q,  vector<vector<bool>>& vis, vector<vector<int>>& grid){
-        int n = grid.size();
-        int m = grid[0].size();
-        int cnt = 0;
-        while(!q.empty()){
-            int size = q.size();
-            for(int k = 0; k<size; k++){
-                auto curr = q.front();
-                int i = curr.first;
-                int j = curr.second;
-                q.pop();
-                if(i+1 <n  &&  !vis[i+1][j]  && grid[i+1][j] == 1){
-                    q.push({i+1, j});
-                    grid[i+1][j] = 2;
-                    vis[i+1][j] = true;
-                }
-                if(i-1 >=0  &&  !vis[i-1][j]  && grid[i-1][j] == 1){
-                    q.push({i-1, j});
-                    grid[i-1][j] = 2;
-                    vis[i-1][j] = true;
-                }
-                if(j+1 <m  &&  !vis[i][j+1]  && grid[i][j+1] == 1){
-                    q.push({i, j+1});
-                    grid[i][j+1] = 2;
-                    vis[i][j+1] = true;
-                }
-                if(j-1 >=0  &&  !vis[i][j-1]  && grid[i][j-1] == 1){
-                    grid[i][j-1] = 2;
-                    q.push({i, j-1});
-                    vis[i][j-1] = true;
-                }
-            }
-            cnt++;
-        }
-        return cnt;
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-        queue <pair<int, int>> q;
         int n = grid.size();
         int m = grid[0].size();
+        queue<pair<pair<int, int>, int>> q;
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j] == 2){
-                    q.push({i, j});
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
                     vis[i][j] = true;
                 }
             }
         }
-        int res = 0;
-        res = bfs(q, vis, grid);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j] == 1){
-                    return -1;
+        int cnt = 0;
+        while(!q.empty()){
+            int els = q.size(); // elements to process before cnt++;
+            for(int k =0; k<els; k++){
+                auto curr = q.front();
+                q.pop();
+                int i = curr.first.first;
+                int j = curr.first.second;
+                cnt = curr.second;
+
+                if(i+1 < n && !vis[i+1][j] && grid[i+1][j]==1){
+                    q.push({{i+1, j}, cnt+1});
+                    vis[i+1][j] = true;
+                }
+                if(i-1>=0 && !vis[i-1][j] && grid[i-1][j]==1){
+                    q.push({{i-1, j}, cnt+1});
+                    vis[i-1][j] = true;
+                }
+                if(j+1 < m && !vis[i][j+1] && grid[i][j+1]==1){
+                    q.push({{i, j+1}, cnt+1});
+                    vis[i][j+1] = true;
+                }
+                if(j-1>=0 && !vis[i][j-1] && grid[i][j-1]==1){
+                    q.push({{i, j-1}, cnt+1});
+                    vis[i][j-1] = true;
                 }
             }
         }
-        if(res == 0) return res;
-        return res-1;
+        for(int i=0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 1 && vis[i][j] == false) return -1;
+            }
+        }
+        return cnt;
     }
 };
