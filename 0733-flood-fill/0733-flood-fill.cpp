d9e288e4dc8b33eaa3e7adca328243ea589f2d23
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-24 13:35:35 +0530

    Time: 0 ms (100%), Space: 18.5 MB (18.34%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,46 +1,17 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& image, int i, int j, int color, int val){
+        if(i<0 || j<0 || i>=image.size() || j>=image[0].size() || image[i][j] != val) return;
+        if(image[i][j] == color) return;
+        image[i][j] = color;
+        dfs(image, i+1, j, color, val);
+        dfs(image, i-1, j, color, val);
+        dfs(image, i, j+1, color, val);
+        dfs(image, i, j-1, color, val);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<bool>> vis(n, vector<bool> (m, false));
-        queue <pair<int, int>> q;
-
-        q.push({sr, sc});
-        vis[sr][sc] = true;
-        int currPixel = image[sr][sc];
-
-        while(!q.empty()){
-            auto curr = q.front();
-            q.pop();
-            int i = curr.first;
-            int j = curr.second;
-
-            image[i][j] = color;
-            if(i+1<n && !vis[i+1][j] &&image[i+1][j]==currPixel){
-                image[i+1][j] = color;
-                vis[i+1][j] = true;
-                q.push({i+1, j});
-            }
-
-            if(i-1>=0 && !vis[i-1][j] &&image[i-1][j]==currPixel){
-                image[i-1][j] = color;
-                vis[i-1][j] = true;
-                q.push({i-1, j});
-            }
-
-            if(j+1<m && !vis[i][j+1] &&image[i][j+1]==currPixel){
-                image[i][j+1] = color;
-                vis[i][j+1] = true;
-                q.push({i, j+1});
-            }
-
-            if(j-1>=0 && !vis[i][j-1] &&image[i][j-1]==currPixel){
-                image[i][j-1] = color;
-                vis[i][j-1] = true;
-                q.push({i, j-1});
-            }
-        }
+        int val = image[sr][sc];
+        dfs(image, sr, sc, color, val);
         return image;
     }
 };
