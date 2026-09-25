a432655040f60d55d6a6d4d00f7d95ae2097765b
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-25 10:41:37 +0530

    Time: 19 ms (54.51%), Space: 38.1 MB (42.53%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    void bfs(int i, int j, vector<vector<int>> &res, vector<vector<int>>& mat, int dist){
-        int n = mat.size();
-        int m = mat[0].size();        
-    }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
@@ -21,33 +17,30 @@ public:
         }
 
         while(!q.empty()){
-            int size = q.size();
-            for(int k =0; k<size; k++){
-                auto curr = q.front();
-                q.pop();
-                int i = curr.first.first;
-                int j = curr.first.second;
-                int dist = curr.second;
-                if(i+1 < n && !vis[i+1][j] && mat[i+1][j]==1){
-                    res[i+1][j] = dist+1;
-                    q.push({{i+1, j}, dist+1});
-                    vis[i+1][j] = true;
-                }
-                if(i-1>=0 && !vis[i-1][j] && mat[i-1][j]==1){
-                    res[i-1][j] = dist+1;
-                    q.push({{i-1, j}, dist+1});
-                    vis[i-1][j] = true;
-                }
-                if(j+1 < m && !vis[i][j+1] && mat[i][j+1]==1){
-                    res[i][j+1] = dist+1;
-                    q.push({{i, j+1}, dist+1});
-                    vis[i][j+1] = true;
-                }
-                if(j-1>=0 && !vis[i][j-1] && mat[i][j-1]==1){
-                    res[i][j-1] = dist+1;
-                    q.push({{i, j-1}, dist+1});
-                    vis[i][j-1] = true;
-                }
+            auto curr = q.front();
+            q.pop();
+            int i = curr.first.first;
+            int j = curr.first.second;
+            int dist = curr.second;
+            if(i+1 < n && !vis[i+1][j] && mat[i+1][j]==1){
+                res[i+1][j] = dist+1;
+                q.push({{i+1, j}, dist+1});
+                vis[i+1][j] = true;
+            }
+            if(i-1>=0 && !vis[i-1][j] && mat[i-1][j]==1){
+                res[i-1][j] = dist+1;
+                q.push({{i-1, j}, dist+1});
+                vis[i-1][j] = true;
+            }
+            if(j+1 < m && !vis[i][j+1] && mat[i][j+1]==1){
+                res[i][j+1] = dist+1;
+                q.push({{i, j+1}, dist+1});
+                vis[i][j+1] = true;
+            }
+            if(j-1>=0 && !vis[i][j-1] && mat[i][j-1]==1){
+                res[i][j-1] = dist+1;
+                q.push({{i, j-1}, dist+1});
+                vis[i][j-1] = true;
             }
         }
         return res;
