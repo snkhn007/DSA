294baa9e107ba915c78639328ce7803ad7125b6f
Sana Sadaf Khan <sanasadafkhan2005@gmail.com>
2026-09-26 12:26:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (62.79%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int i =0, j =0, n=word1.size(), m=word2.size();
+        while(i < n && j<m){
+            res+=word1[i++];
+            res+=word2[j++];
+        }
+        while(i < n){
+            res+=word1[i++];
+        }
+        while(j<m){
+            res+=word2[j++];
+        }
+
+        return res;
+    }
+};
